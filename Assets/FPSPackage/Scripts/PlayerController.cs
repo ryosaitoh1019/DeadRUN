084a8477017522a0092cc247ab26c8f2d84471d9
DESKTOP-EOAUAR8\infin <infinityfreedom1019@gmail.com>
2018-05-05 00:02:37 +0900

    出来たこと（当たり判定・銃の実装・プレイヤーの基本的な動き） 出来なかったこと（ＨＰ減少しない・敵が動いてくれない・scoreスクリプト未実装・ダメージ判定） なぜできなかったのか、鬼宿題のようにやればできるとおもっていたがキャラが変わるので全部同じ通りでできず終わらなかった。できる時間あったのに少し遊んでたので取り組む気持ちが足りなかったです。日曜日には終わらせます。

---
+++ Assets/FPSPackage/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera camera;
+
+    public AudioSource gunSound;
+
+    int playerHP = 10;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+            gunSound.Play();
+        }
+    }
+    void Shot()
+    {
+        int distance = 10;
+        Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, distance))
+        {
+            if (hitInfo.collider.tag == "Enemy")
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+
+        }
+    }
+}
